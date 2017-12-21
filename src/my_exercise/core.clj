f0c9a8acd5411980e57f9f386186aef5022ecc85
Anonymous Applicant <anonymous@example.com>
2017-12-21 14:25:29 -0500

    Add search route. Add search page fn with bad view

--- src/my_exercise/core.clj
+++ src/my_exercise/core.clj
@@ -3,10 +3,12 @@
             [compojure.route :as route]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
             [ring.middleware.reload :refer [wrap-reload]]
-            [my-exercise.home :as home]))
+            [my-exercise.home :as home]
+            [my-exercise.search :as search]))
 
 (defroutes app
   (GET "/" [] home/page)
+  (POST "/search" [] search/page)
   (route/resources "/")
   (route/not-found "Not found"))
 
--- src/my_exercise/search.clj
+++ src/my_exercise/search.clj
@@ -1,1 +1,8 @@
-(ns my-exercise.search)
+(ns my-exercise.search
+  (:require [hiccup.page :refer [html5]]))
+
+(defn page
+  "Create the search page view from the request"
+  [request]
+  (html5
+    [:div "Hello from search!"]))
