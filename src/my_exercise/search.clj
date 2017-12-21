bb57d85f910271e306770c835991765726c0b604
Anonymous Applicant <anonymous@example.com>
2017-12-21 14:54:37 -0500

    Add OCD ID multimethod

--- src/my_exercise/search.clj
+++ src/my_exercise/search.clj
@@ -1,15 +1,38 @@
 (ns my-exercise.search
   (:require [hiccup.page :refer [html5]]))
 
+
 (defn search-params
   "Select only the valid search params.
   For this exercise, it'll be city and state from the form."
   [m]
   (select-keys m [:city :state]))
 
+(defmulti ocd-ids (fn [m] (set (keys m))))
+
+(defmethod ocd-ids #{:city :state}
+  [{:keys [city state] :as params}]
+  [(format "ocd-division/country:us/state:%s/place:%s" state city)
+   (format "ocd-division/country:us/state:%s" state)
+   "ocd-division/country:us"])
+
+(defmethod ocd-ids #{:state}
+  [{:keys [state] :as params}]
+  [(format "ocd-division/country:us/state:%s" state)
+   "ocd-division/country:us"])
+
+(defmethod ocd-ids :default [params] ["ocd-division/country:us"])
+
+(defn build-ocd-ids
+  [params]
+  (->> params
+       search-params
+       (into {} (filter (comp some? seq val)))
+       ocd-ids))
+
 (defn fetch-upcoming-elections
   [{:keys [params] :as request}]
-  (str (search-params params)))
+  (str (build-ocd-ids (search-params params))))
 
 (defn upcoming-elections
   [request]
